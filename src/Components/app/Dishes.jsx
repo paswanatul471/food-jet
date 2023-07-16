f202e5e071437b8d417cd65be73d84e301c47939
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-07-16 21:34:53 +0530

    Updated

--- src/Components/app/Dishes.jsx
+++ src/Components/app/Dishes.jsx
@@ -9,19 +9,23 @@ import img6 from "../assets/noodles.jpg";
 function Dishes() {
   return (
     <>
-      <div className="px-20">
+      <div className="px-20 max-sm:px-2 max-md:px-2">
         <div className="my-12">
           <div className="flex justify-between">
-          <p className="order-first text-4xl font-semibold">Popular Recipes Of The Week</p>
-          <span className="text-green-800 font-semibold text-2xl">
-            <a href="#">see more</a>
-          </span>
+            <p className="order-first text-4xl font-semibold">
+              Popular Recipes Of The Week
+            </p>
+            <span className="text-green-800 font-semibold text-2xl">
+              <a href="#">see more</a>
+            </span>
           </div>
-          
-          <p className="my-3 text-[#888888] text-2xl">Our most popular recipes of this week</p>
+
+          <p className="my-3 text-[#888888] text-2xl">
+            Our most popular recipes of this week
+          </p>
         </div>
 
-        <div className="">
+        {/* <div className="">
           <div className="flex flex-row sm:flex-row space-x-5">
             <div className="">
               <img
@@ -81,6 +85,39 @@ function Dishes() {
                 <span className="text-xl">By Smith Jhon</span>
               </div>
             </div>
+          </div>
+        </div> */}
+
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          <div>
+            <img src={img1} alt="Please wait" className="w-full" />
+            <h1 className="text-xl font-bold mt-2">Fried Beans</h1>
+            <span className="text-gray-600">By Garry Don</span>
+          </div>
+          <div>
+            <img src={img2} alt="Please wait" className="w-full" />
+            <h1 className="text-xl font-bold mt-2">Cheesy Chips</h1>
+            <span className="text-gray-600">By Charie Laos</span>
+          </div>
+          <div>
+            <img src={img3} alt="Please wait" className="w-full" />
+            <h1 className="text-xl font-bold mt-2">Creamy Pancake</h1>
+            <span className="text-gray-600">By Luise De</span>
+          </div>
+          <div>
+            <img src={img4} alt="Please wait" className="w-full" />
+            <h1 className="text-xl font-bold mt-2">Coated Onion</h1>
+            <span className="text-gray-600">By AJ</span>
+          </div>
+          <div>
+            <img src={img5} alt="Please wait" className="w-full" />
+            <h1 className="text-xl font-bold mt-2">Vegan Soup</h1>
+            <span className="text-gray-600">By Jennifer Lay</span>
+          </div>
+          <div>
+            <img src={img6} alt="Please wait" className="w-full" />
+            <h1 className="text-xl font-bold mt-2">Juicy Noodles</h1>
+            <span className="text-gray-600">By Smith Jhon</span>
           </div>
         </div>
       </div>
